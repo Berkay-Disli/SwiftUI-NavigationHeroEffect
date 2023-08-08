e3d3a82c0da75e10cb21a87138eba41c197c1bb1
Berkay Dişli <berkay@abonesepeti.com>
2023-08-08 17:09:24 +0300

    Initial Commit

---
+++ NavigationHeroEffect/NavigationHeroEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationHeroEffectApp.swift
+//  NavigationHeroEffect
+//
+//  Created by Berkay Dişli on 8.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationHeroEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
